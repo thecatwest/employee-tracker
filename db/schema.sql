9a659de8cba5a2a42a7ecb5c9070a8daafca1d9c
TheCatWest <catwestover@outlook.com>
2021-05-03 10:53:42 -0700

    set up express.js connection

--- db/schema.sql
+++ db/schema.sql
@@ -5,13 +5,12 @@ CREATE TABLE department (
 CREATE TABLE roles (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL(4,2) NOT NULL,
+    salary DECIMAL NOT NULL,
     department_id INT NOT NULL
 );
 CREATE TABLE employee (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30),
-    role_id INT NOT NULL,
-    manager_id INT
+    role_id INT NOT NULL
 );
