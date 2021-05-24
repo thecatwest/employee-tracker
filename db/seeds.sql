a7c9419ff0456b7b9ded05bd9065ea92060ad090
TheCatWest <catwestover@outlook.com>
2021-05-24 14:09:16 -0700

    deleted useless code; set up connection.js

--- db/seeds.sql
+++ db/seeds.sql
@@ -16,3 +16,6 @@ VALUES ('Caretaker', 35000.00, 2),
     ('Astronomy', 110000.00, 3),
     ('Potions', 90000.00, 3),
     ('Herbology', 90000.00, 3);
+INSERT INTO manager (first_name, last_name)
+VALUES ('Albus', 'Dumbledore'),
+    ('Minerva', 'McGonagall');
--- server.js
+++ server.js
@@ -1,146 +1,31 @@
 // import mysql2
 const mysql = require("mysql2");
+const inquirer = require("inquirer");
 
-// import express
-const express = require("express");
-
-// add PORT designation and app express
-const PORT = process.env.PORT || 3001;
-const app = express();
-
-// add express.js middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+const database = require("./db/db.sql");
+const mysql = require("mysql2");
 
 // connect application to MySQL database
-const db = mysql.createConnection(
-  {
-    host: "localhost",
-    // MySQL username
-    user: "root",
-    // MySQL password
-    password: "poop",
-    database: "employee_tracker",
-  },
-  console.log("Connected to the employee-tracker database.")
-);
-
-// test the connection with GET test route
-// app.get('/', (req, res) => {
-//     res.json({
-//         message: 'Hello World'
-//     });
-// });
-
-db.query(`SELECT * FROM employee`, (err, rows) => {
-  console.log(rows);
+const db = mysql.createConnection({
+  host: "localhost",
+  // MySQL username
+  user: "root",
+  // MySQL password
+  password: "",
+  database: "employee_tracker",
 });
 
-// GET single employee
-app.get("/api/employee/:id", (req, res) => {
-  const sql = `SELECT * FROM employee WHERE id = ?`;
-  const params = [req.params.id];
+// TODO
+// function to prompt user to choose between:
+// view all departments
+// view all employees
+// view all roles
+// update employee
+// update role
+// add department
+// add employee
+// add role
 
-  db.query(sql, params, (err, row) => {
-    if (err) {
-      res.status(400).json({ error: err.message });
-      return;
-    }
-    res.json({
-      message: "success",
-      data: row,
-    });
-  });
-});
 
-// DELETE single employee
-app.delete("/api/employee/:id", (req, res) => {
-  const sql = `DELETE FROM employee WHERE id = ?`;
-  const params = [req.params.id];
 
-  db.query(sql, params, (err, result) => {
-    if (err) {
-      res.statusMessage(400).json({ error: message });
-    } else if (!result.affectedRows) {
-      res.json({
-        message: "deleted",
-        changes: result.affectedRows,
-        id: req.params.id,
-      });
-    }
-  });
-});
 
-// CREATE new employee
-// set SQL command and parameters as variables to improve legibility
-// const sql = `INSERT INTO employee (id, first_name, last_name, role_id)
-//     VALUES (?,?,?,?)`;
-// const params = [1, "Argus", "Filch", 2];
-
-// db.query(sql, params, (err, result) => {
-//   if (err) {
-//     console.log(err);
-//   }
-//   console.log(result);
-// });
-
-// CREATE new employee
-// app.post("/api/employee", ({ body }, res) => {
-//   const errors = inputCheck(
-//     body,
-//     "first_name",
-//     "last_name",
-//     "role_id",
-//     "manager_id"
-//   );
-//   if (errors) {
-//     res.status(400).json({ error: errors });
-//     return;
-//   }
-//   const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id)
-//         VALUES (?,?,?,?)`;
-//   const params = [
-//     body.first_name,
-//     body.last_name,
-//     body.role_id,
-//     body.manager_id,
-//   ];
-
-//   db.query(sql, params, (err, result) => {
-//     if (err) {
-//       res.status(400).json({ error: err.message });
-//       return;
-//     }
-//     res.json({
-//       message: "success",
-//       data: body,
-//     });
-//   });
-// });
-
-// GET all employees
-app.get("/api/employee", (req, res) => {
-  const sql = `SELECT * FROM employee`;
-
-  db.query(sql, (err, rows) => {
-    if (err) {
-      res.status(500).json({ error: err.message });
-      return;
-    }
-    res.json({
-      message: "success",
-      data: rows,
-    });
-  });
-});
-
-// add route to handle user requests that aren't supported by app
-// this will override GET routes - make sure it's last of app.uses
-app.use((req, res) => {
-  res.status(404).end();
-});
-
-// add function that will start express.js server on port 3001
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}!`);
-});
