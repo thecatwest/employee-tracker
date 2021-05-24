0b7c05cfb67b7a137ead4b60baf8beaac048f37b
TheCatWest <catwestover@outlook.com>
2021-05-24 15:32:48 -0700

    restructured employee roles to work better in context

--- db/seeds.sql
+++ db/seeds.sql
@@ -7,15 +7,9 @@ VALUES ('Caretaker', 35000.00, 2),
     ('Grounds Keeper', 50000.00, 2),
     ('Head Master', 300000.00, 1),
     ('Head Mistress', 350000.00, 1),
-    ('Divinations', 80000.00, 3),
-    ('Charms', 85000.00, 3),
-    ('Care of Magical Creatures', 90000.00, 3),
-    ('Flying', 100000.00, 3),
-    ('Defence Against the Dark Arts', 120000.00, 3),
-    ('Transfiguration', 150000.00, 3),
-    ('Astronomy', 110000.00, 3),
-    ('Potions', 90000.00, 3),
-    ('Herbology', 90000.00, 3);
+    ('Professor I', 80000.00, 3),
+    ('Professor II', 85000.00, 3),
+    ('Professor III', 90000.00, 3);
 INSERT INTO manager (first_name, last_name)
 VALUES ('Albus', 'Dumbledore'),
     ('Minerva', 'McGonagall');
